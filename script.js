d415a8ee400487d3830239ef7ed6b6bd1fff1335
hunar <hunar0115@gmail.com>
2022-11-02 09:55:49 +0600

    test udemy

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log('im script');
+// console.log('im script');
+const numberofFilms=prompt('сколько филмов вы уже посмотрели ?','');
+const personalMovieDB={
+    count:numberofFilms,
+    movies:{},
+    actors:{},
+    genres:[],
+    privat:false,
+
+};
+const a = prompt('один из последних просмотренных филмов ?',''),
+    b = prompt('на сколько оцените его?',''),
+    c = prompt('один из последних просмотренных филмов ?',''),
+    d = prompt('на сколько оцените его?','');
+    personalMovieDB.movies[a]=b;
+    personalMovieDB.movies[c]=d;
+    console.log(personalMovieDB);
