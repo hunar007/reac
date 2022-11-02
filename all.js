eb17a3f15e90ceae3477730b0dc9c3d0ee5543e5
hunar <hunar0115@gmail.com>
2022-11-02 13:35:15 +0600

    new knowledg

--- all.js
+++ all.js
@@ -1,2 +1,53 @@
-console.log('im all');
-console.log('wtf');
+// console.log('im all');
+// console.log('wtf');
+// if(4==9){
+//     console.log('ok');
+// }else{
+//     console.log('error');
+// }
+// const num=50;
+// if(num<49){
+//     console.log('error');
+// }else if(num>100){
+//     console.log('mnogo');
+// }else{
+//     console.log('ok!');
+// }
+// (num===50)? console.log('ok!'):console.log('erroe');
+// switch(num){
+//     case 49:
+//         console.log('no');
+//         break;
+//     case 100:
+//         console.log('no1');
+//         break;
+//     case 50:
+//         console.log('yeah');
+//         break;
+//     default:
+//         console.log('not this round');
+//         break;
+
+// }
+// const hamburger=true;
+// const fries=true;
+// if(hamburger&&fries){
+//     console.log('я сыт!');
+// }
+// console.log((hamburger&&fries));
+
+// const hamburger=2;
+// const fries=1;
+// const cola= 2;
+// const nuggest=3;
+// if(hamburger === 3 && fries && cola===2 && nuggest===3){
+//     console.log('all сыт!');
+// }else{
+//     console.log('go out ');
+// }
+"use strict";
+const num = 50;
+while (num <= 55) {
+    console.log(num);
+    num++;
+}
