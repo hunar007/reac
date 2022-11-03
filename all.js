c698cb5c28225c091570ddddbc7c757d604875d6
hunar <hunar0115@gmail.com>
2022-11-03 12:57:04 +0600

    last urok

--- all.js
+++ all.js
@@ -45,9 +45,145 @@
 // }else{
 //     console.log('go out ');
 // }
-"use strict";
-const num = 50;
-while (num <= 55) {
-    console.log(num);
-    num++;
-}
+
+// let num = 50;
+// while (num <= 55) {
+//     console.log(num);
+//     num++;
+// }
+
+// do {
+//     console.log(num);
+//     num++;
+// }
+// while(num<55);
+
+// for(let i=1;i<num;i++){
+//     console.log(i);
+// }
+
+// for(i=1;i<10;i++){
+//    if(i===6){
+//         // break;
+//         continue;
+//    }
+//    console.log(i);
+// }
+// for(let i=0;i<3;i++){
+//     console.log(i);
+//     for(let j=0;j<3;j++){
+//         console.log(j);
+//     }
+// }
+// let result='';
+// const length=7;
+
+// for(let i=1;i<length;i++){
+//     for(let j=0;j<i;j++){
+//         result+='*';
+//     }
+//     result+='\n';
+// }
+
+
+// console.log(result);
+
+// first:for(let i=0;i<3;i++){
+//     console.log(`First level:${i}`);
+//     for(let j=0;j<3;j++){
+//         console.log(`Second level:${j}`);
+//         for(let k=0;k<3;k++){
+//             if(k===2)continue first; //break
+//             console.log(`Third level:${k}`);
+    
+//         }
+
+//     }
+// }
+
+// for(let i=20;i > 10; i--){
+//     if(i===13){
+//         break;
+//     }
+//     console.log(i);
+// }
+// for(let i=2;i<=10;i++){
+//     if (i % 2 == 0) {
+//         console.log(i);
+//      }
+     
+//  }
+
+// const arrayOfNumbers = [];
+// let i=0;
+// for(let j=5;j<=10;j++){
+//     arrayOfNumbers[i++]=j;
+// }
+// console.log(arrayOfNumbers);
+
+// const arr = [3, 5, 8, 16, 20, 23, 50];
+// const result = [];
+// for(let i=0;i<arr.length;i++){
+//     // console.log( arr[i]);
+//     result.push(arr[i])
+    
+// }
+// console.log(result);
+
+// const data = [5, 10, 'Shopping', 20, 'Homework'];
+
+
+//     for(let i=0;i<data.length;i++){
+        
+//         if(typeof(data[i]) !='string'){
+//             data[i]=data[i]*2;
+//         }else{
+//             data[i]=data[i]+'-done';
+//         }
+//     }
+//     console.log(data);
+
+// const data = [5, 10, 'Shopping', 20, 'Homework'];
+// const result = [];
+// let j=0;
+// // Пишем решение вот тут
+// console.log(data.length);
+// for(let i=data.length-1;i<data.length;i--){
+//     console.log(data[i]);
+//     if(i<=-1)break;result[j++]=data[i];
+ 
+// }
+// console.log(result);
+    
+// const lines = 5;
+// let result = '';
+// // Проверяется именно переменная result, формируйте строку в ней
+// for (let i = 0; i <= lines; i++) {
+//     for (let j = 0; j < lines - i; j++) {
+//         result += " ";
+//     }
+//     for (let j = 0; j < 2 * i + 1; j++) {
+//         result += "*";
+//     }
+//     result += "\n";
+// }
+
+// console.log(result);
+
+
+// function showFirstMessage(text){
+//     console.log(text);
+// }
+// showFirstMessage('Hello World!');
+
+// function calc(a,b){
+//     return a+b;
+// }
+// console.log(calc(4,3));
+// console.log(calc(5,6));
+// console.log(calc(10,6));
+
+const looger=function(){
+    console.log('hello');
+};
+looger();
--- script.js
+++ script.js
@@ -8,10 +8,32 @@ const personalMovieDB={
     privat:false,
 
 };
-const a = prompt('один из последних просмотренных филмов ?',''),
-    b = prompt('на сколько оцените его?',''),
-    c = prompt('один из последних просмотренных филмов ?',''),
-    d = prompt('на сколько оцените его?','');
-    personalMovieDB.movies[a]=b;
-    personalMovieDB.movies[c]=d;
+// const a = prompt('один из последних просмотренных филмов ?',''),
+//     b = prompt('на сколько оцените его?',''),
+//     c = prompt('один из последних просмотренных филмов ?',''),
+//     d = prompt('на сколько оцените его?','');
+//     personalMovieDB.movies[a]=b;
+//     personalMovieDB.movies[c]=d;
+
+    for(let i=0;i<2;i++){
+        const a = prompt('один из последних просмотренных филмов ?',''),
+              b = prompt('на сколько оцените его?','');
+              
+        if(a != null && b !== null && a !=='' && b !='' && a.length<50){
+            personalMovieDB.movies[a]=b;
+            console.log('done');
+        }else{
+            console.log('error');
+            i--;
+        }
+    }
+    if(personalMovieDB.count >= 10 && personalMovieDB.count <= 30){
+        console.log('Посмотрено довоьно мало филмов');
+    }else if(personalMovieDB.count){
+        console.log('вы классический зрител');
+    }else if (personalMovieDB.count >= 30){
+        console.log('вы киноман');
+    }else{
+        console.log('призашло ошибка');
+    }
     console.log(personalMovieDB);
