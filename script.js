e171c912a035ab2a3df6f4abd821e795c821a23c
hunar <hunar0115@gmail.com>
2022-11-08 17:32:56 +0600

    safaef

--- script.js
+++ script.js
@@ -37,4 +37,71 @@
     //     console.log('призашло ошибка');
     // }
     // console.log(personalMovieDB);
+
+    // function first(){
+    //     setTimeout(function(){
+    //         console.log(1);
+    //     },500);
+    // }
     
+    // function second(){
+    //     console.log(2);
+    // }
+    // first();
+    // second();
+
+    // function learnJS(lang,callback){
+    //     console.log(`i am learning ${lang}`);
+    //     callback();
+    // }
+
+    // function done(){
+    //     console.log('ive done course');
+    // }
+
+    // // function learnJS(lang,callback){
+    // //     console.log(`i am learning ${lang}`);
+    // // }
+    // // learnJS('JavaScript',function(){
+    // //     console.log('i`ve begaining course');
+    // // });
+
+    // learnJS('JavaScript', done);
+    
+    // const obj = new Object();
+
+    const options ={
+        name:'test',
+        width:1024,
+        height:1024,
+        colors:{
+            boder:'black',
+            bg:'red',
+        },
+        makeTest: function(){
+            console.log('test');
+
+        }
+    };
+    // console.log(options.name);
+    // delete options.name;
+    // console.log(options);
+
+    // let counter = 0;
+    // for(let key in options){
+    //     if(typeof(options[key])==='object'){
+    //         for(let i in options[key]){
+    //             console.log(`key is ${i} value is ${options[key][i]}`);
+    //             counter++;
+    //         }
+
+    //     }else{
+    //         console.log(`key is ${key} value is ${options[key]}`);
+    //         counter++;
+    //     }
+    // }
+    // console.log(counter);
+    options.makeTest();
+    // console.log(Object.keys(options).length);
+    const{boder ,bg}= options.colors;
+    console.log(boder);
